bbdae37c2be8b1a9be65f0ce86b638c68918fc29
hp <sathiya.kumaresan@aspiresys.com>
2021-01-05 20:36:30 +0530

    creating custom widget

---
+++ app/code/TrendsAttribute/CustomWidget/Block/Widget/Offerwidget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TrendsAttribute\CustomWidget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Offerwidget extends Template implements BlockInterface
+{
+    protected $_template = "widget/offerwidget.phtml";
+}
