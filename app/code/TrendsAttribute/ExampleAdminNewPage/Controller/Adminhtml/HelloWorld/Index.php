df58feeaaadf5189a87a95f99528f4fbed7d36bc
hp <sathiya.kumaresan@aspiresys.com>
2021-01-12 20:37:10 +0530

    admin menu

--- app/code/TrendsAttribute/ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/Index.php
+++ app/code/TrendsAttribute/ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/Index.php
@@ -43,7 +43,6 @@ class Index extends Action implements HttpGetActionInterface
     {
         $resultPage = $this->resultPageFactory->create();
         $resultPage->setActiveMenu(static::MENU_ID);
-        $resultPage->getConfig()->getTitle()->prepend(__('Hello World'));
 
         return $resultPage;
     }
