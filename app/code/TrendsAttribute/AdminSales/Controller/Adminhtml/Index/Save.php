faf0b46b79f9aab2f3136089e7605d9a6571d58b
hp <sathiya.kumaresan@aspiresys.com>
2021-02-03 22:19:46 +0530

    save

---
+++ app/code/TrendsAttribute/AdminSales/Controller/Adminhtml/Index/Save.php
@@ -0,0 +1,50 @@
+<?php
+namespace TrendsAttribute\AdminSales\Controller\Adminhtml\Index;
+
+class Save extends \Magento\Backend\App\Action
+{
+    protected $_itemFactory;
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \TrendsAttribute\AdminSales\Model\ItemFactory $itemFactory
+    ) {
+        $this->_itemFactory = $itemFactory;
+        parent::__construct($context);
+    }
+    /**
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+
+        if (!$data) {
+            $this->_redirect('adminsales/index/index');
+            return;
+        }
+        try {
+            $rowData = $this->_itemFactory->create()->load($data['item_id']);
+
+            if (!$rowData->getId()) {
+                $this->messageManager->addError(__('row data no longer exist.'));
+                $this->_redirect('adminsales/index/index');
+                return;
+            }
+            $rowData->setData('change_status', $data['change_status']);
+            $rowData->save();
+            $this->messageManager->addSuccess(__('Row data has been successfully saved.'));
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__($e->getMessage()));
+        }
+        $this->_redirect("*/*/");
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('TrendsAttribute_AdminSales::productsales');
+    }
+}
