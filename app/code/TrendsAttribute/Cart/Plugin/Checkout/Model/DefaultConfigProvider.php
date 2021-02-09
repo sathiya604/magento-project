0a848441e1ae54650812c2bab51faa73d81baa82
hp <sathiya.kumaresan@aspiresys.com>
2021-02-09 20:34:24 +0530

    adding custom attribute in order summary

---
+++ app/code/TrendsAttribute/Cart/Plugin/Checkout/Model/DefaultConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace TrendsAttribute\Cart\Plugin\Checkout\Model;
+
+use Magento\Checkout\Model\Session as CheckoutSession;
+
+class DefaultConfigProvider
+{
+    /**
+     * @var CheckoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * Constructor
+     *
+     * @param CheckoutSession $checkoutSession
+     */
+    public function __construct(
+        CheckoutSession $checkoutSession
+    ) {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    public function afterGetConfig(
+        \Magento\Checkout\Model\DefaultConfigProvider $subject,
+        array $result
+    ) {
+        $items = $result['totalsData']['items'];
+        foreach ($items as $index => $item) {
+            $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
+            $result['quoteItemData'][$index]['clothing_material'] = $quoteItem->getProduct()->getAttributeText('clothing_material');
+        }
+        return $result;
+    }
+}
