bb4a28d4b08a689445edf99f623e62164b697447
hp <sathiya.kumaresan@aspiresys.com>
2021-02-24 21:45:22 +0530

    Api and curl

---
+++ app/code/Task/CurlApi/Helper/Inventory.php
@@ -0,0 +1,62 @@
+<?php
+namespace Task\CurlApi\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class Inventory extends AbstractHelper
+{
+
+    public function construct(Psr\Log\LoggerInterface $log)
+    {
+       $this->log = $log;
+    }
+
+    public function makeACurlRequest()
+    {
+        $username = 'sathiya';
+        $password = 'Sarojini@123';
+        $tokenUrl = 'http://magento-dev.com/rest/V1/integration/admin/token';
+        $ch = curl_init();
+        $data = ["username" => $username, "password" => $password];
+        $data_string = json_encode($data);
+
+        $ch = curl_init($tokenUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            [
+               'Content-Type: application/json',
+               'Content-Length: ' . strlen($data_string)]
+        );
+        $token = curl_exec($ch);
+        $ch = curl_init("http://magento-dev.com/rest/arabia/V1/customconfig/config");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json", "Authorization: Bearer " . json_decode($token)]);
+        $result = curl_exec($ch);
+  
+        $httpcode = curl_getinfo($ch);
+        $this->log->info($httpcode);
+        if ($httpcode['http_code'] == 400) {
+            return 'Kindly check the parameters';
+        } elseif ($httpcode['http_code'] == 401) {
+            return 'Credentials are Missing';
+        } elseif ($httpcode['http_code'] == 403) {
+            return 'Forbidden';
+        } elseif ($httpcode['http_code'] == 404) {
+            return 'URL Not Found';
+        } elseif ($httpcode['http_code'] == 405) {
+            return 'Sorry! Not Allowed';
+        } elseif ($httpcode['http_code'] == 406) {
+            return 'Sorry! Not Acceptable';
+        } elseif ($httpcode['http_code'] == 500) {
+            return 'Internal Error! Contact Admin';
+        } else {
+            return $result;
+        }
+
+    }
+}
