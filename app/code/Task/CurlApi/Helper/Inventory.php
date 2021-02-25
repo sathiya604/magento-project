465b0492178216cf79ad0a52db09d888ba551232
hp <sathiya.kumaresan@aspiresys.com>
2021-02-25 21:01:59 +0530

    Api with exceptions

--- app/code/Task/CurlApi/Helper/Inventory.php
+++ app/code/Task/CurlApi/Helper/Inventory.php
@@ -5,15 +5,9 @@ use Magento\Framework\App\Helper\AbstractHelper;
 
 class Inventory extends AbstractHelper
 {
-
-    public function construct(Psr\Log\LoggerInterface $log)
-    {
-       $this->log = $log;
-    }
-
     public function makeACurlRequest()
     {
-        $username = 'sathiya';
+        $username = 'admin';
         $password = 'Sarojini@123';
         $tokenUrl = 'http://magento-dev.com/rest/V1/integration/admin/token';
         $ch = curl_init();
@@ -32,14 +26,29 @@ class Inventory extends AbstractHelper
                'Content-Length: ' . strlen($data_string)]
         );
         $token = curl_exec($ch);
+        $httpcode = curl_getinfo($ch);
+        if ($httpcode['http_code'] == 400) {
+            return 'Token: Kindly check the parameters';
+        } elseif ($httpcode['http_code'] == 401) {
+            return 'Token: Credentials are Incorrect';
+        } elseif ($httpcode['http_code'] == 403) {
+            return 'Token: Forbidden';
+        } elseif ($httpcode['http_code'] == 404) {
+            return 'Token: URL Not Found';
+        } elseif ($httpcode['http_code'] == 405) {
+            return 'Token: Sorry! Not Allowed';
+        } elseif ($httpcode['http_code'] == 406) {
+            return 'Token: Sorry! Not Acceptable';
+        } elseif ($httpcode['http_code'] == 500) {
+            return 'Token: Internal Error! Contact Admin';
+        }
+
         $ch = curl_init("http://magento-dev.com/rest/arabia/V1/customconfig/config");
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json", "Authorization: Bearer " . json_decode($token)]);
         $result = curl_exec($ch);
-  
         $httpcode = curl_getinfo($ch);
-        $this->log->info($httpcode);
         if ($httpcode['http_code'] == 400) {
             return 'Kindly check the parameters';
         } elseif ($httpcode['http_code'] == 401) {
@@ -57,6 +66,5 @@ class Inventory extends AbstractHelper
         } else {
             return $result;
         }
-
     }
 }
