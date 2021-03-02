d5a4736dea389dbb787baae20c6059c491c79e08
hp <sathiya.kumaresan@aspiresys.com>
2021-03-02 18:54:49 +0530

    Flash Sale using UI component

---
+++ app/code/Task/FlashSale/Model/ResourceModel/Grid/Collection.php
@@ -0,0 +1,58 @@
+<?php
+namespace Task\FlashSale\Model\ResourceModel\Grid;
+
+/* use required classes */
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'item_id';
+
+    /**
+     * @param EntityFactoryInterface $entityFactory,
+     * @param LoggerInterface        $logger,
+     * @param FetchStrategyInterface $fetchStrategy,
+     * @param ManagerInterface       $eventManager,
+     * @param StoreManagerInterface  $storeManager,
+     * @param AdapterInterface       $connection,
+     * @param AbstractDb             $resource
+     */
+    public function __construct(
+        EntityFactoryInterface $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        StoreManagerInterface $storeManager,
+        AdapterInterface $connection = null,
+        AbstractDb $resource = null
+    ) {
+        $this->_init('Task\FlashSale\Model\Sales', 'Task\FlashSale\Model\ResourceModel\Sales');
+        //Class naming structure
+        // 'NameSpace\ModuleName\Model\ModelName', 'NameSpace\ModuleName\Model\ResourceModel\ModelName'
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
+        $this->storeManager = $storeManager;
+    }
+
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+
+        $this->getSelect()
+            ->joinLeft(
+                ['second_table' => $this->getTable('flash_sale_item')],
+                'second_table.flash_sale_id = main_table.flash_sale_id',
+                ['sku' => 'sku', 'qty' => 'qty', 'qty_left' => 'qty_left']
+            );
+
+        return $this;
+    }
+}
