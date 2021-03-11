3366ee8e82f588a4ec24cb864faf9f9f28e3301e
hp <sathiya.kumaresan@aspiresys.com>
2021-03-11 19:46:29 +0530

    flash sale message in catalog page for simple, config, grouped, bundle product

--- app/code/Task/FlashSale/Block/FlashSale.php
+++ app/code/Task/FlashSale/Block/FlashSale.php
@@ -8,6 +8,8 @@ class FlashSale extends \Magento\Framework\View\Element\Template
     protected $itemCollectionFactory;
     protected $dateTime;
     protected $registry;
+    public $flashSale;
+    protected $_productloader;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
@@ -15,13 +17,15 @@ class FlashSale extends \Magento\Framework\View\Element\Template
         \Task\FlashSale\Model\ResourceModel\Grid\ItemCollectionFactory $itemCollectionFactory,
         \Task\FlashSale\Model\ResourceModel\Grid\SalesCollectionFactory $salesCollectionFactory,
         \Magento\Framework\Registry $registry,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $dateTime
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $dateTime,
+        \Magento\Catalog\Model\ProductFactory $_productloader
     ) {
         $this->salesCollectionFactory = $salesCollectionFactory;
         $this->itemCollectionFactory = $itemCollectionFactory;
         $this->flashSaleCollectionFactory = $flashSaleCollectionFactory;
         $this->dateTime = $dateTime;
         $this->registry = $registry;
+        $this->_productloader = $_productloader;
         parent::__construct($context);
     }
 
@@ -42,9 +46,9 @@ class FlashSale extends \Magento\Framework\View\Element\Template
                           ->addFieldToFilter('start_datetime', ['lteq' => $this->dateTime->date()->format('Y-m-d H:i:s')])
                           ->addFieldToFilter('end_datetime', ['gteq' => $this->dateTime->date()->format('Y-m-d H:i:s')])
                           ->addFieldToFilter('is_active', ['eq' => 1]);
-        $flashSale = $collection->getData();
+        $this->flashSale = $collection->getData();
 
-        return $flashSale;
+        return $this->flashSale;
     }
 
     public function flashSaleRemainingTime()
@@ -69,17 +73,72 @@ class FlashSale extends \Magento\Framework\View\Element\Template
         return $this->registry->registry('current_product');
     }
 
+    public function getLoadProduct($id)
+    {
+        return $this->_productloader->create()->load($id);
+    }
+
     public function getConfigurableProduct()
     {
         $product = $this->getCurrentProduct();
-
+        $i =0;
         if ($product->getTypeId() == "configurable") {
             $productTypeInstance = $product->getTypeInstance();
             $usedProducts = $productTypeInstance->getUsedProducts($product);
+            foreach ($usedProducts as  $value) {
+                foreach ($this->flashSaleItems() as $flashSku) {
+                    if ($value->getSku() == $flashSku['sku']) {
+                        $sku[$i]['sku'] = $value->getSku();
+                        $sku[$i]['finalprice'] = $value->getFinalPrice();
+                    }
+                }
+                $i++;
+            }
+        }
 
-            return $usedProducts;
-        } else {
-            return $product->getTypeId();
+        return $sku;
+    }
+
+    public function getGroupedProduct()
+    {
+        $i = 0;
+        $product = $this->getCurrentProduct();
+        if ($product->getTypeId() == "grouped") {
+            $products = $product->getTypeInstance()->getAssociatedProducts($product);
+            foreach ($products as $value) {
+                foreach ($this->flashSaleItems() as $flashSku) {
+                    if ($value->getSku() == $flashSku['sku']) {
+                        $sku[$i]['sku'] = $value->getSku();
+                        $sku[$i]['finalprice'] = $value->getFinalPrice();
+                    }
+                }
+                $i++;
+            }
+        }
+
+        return $sku;
+    }
+
+    public function getBundleProduct()
+    {
+        $i = 0;
+        $product = $this->getCurrentProduct();
+        if ($product->getTypeId() == "bundle") {
+            $products = $product->getTypeInstance()->getChildrenIds($product->getId());
+            foreach ($products as $key => $value) {
+                foreach ($value as $value) {
+                    $product=$this->getLoadProduct($value);
+                    foreach ($this->flashSaleItems() as $flashSku) {
+                        if ($product->getSku() == $flashSku['sku']) {
+                            $sku[$i]['sku'] = $product->getSku();
+                            $sku[$i]['finalprice'] = $product->getFinalPrice();
+                        }
+                    }
+                    $i++;
+                }
+            }
+
+            return $sku;
         }
     }
 }
